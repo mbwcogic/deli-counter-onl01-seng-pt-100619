28151ef166793dde655b8b9ac8eb98372d405bca
Michael Waldo <mbwcogic@yahoo.com>
2019-10-18 22:34:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ puts "The line is currently empty."
  else
    string = "The line is currently:"
    array.each_with_index do |name, index|
-    string += "#{index}. #{name}"
+    string +=  "#{index}. #{name}"
  end
 puts string
  end

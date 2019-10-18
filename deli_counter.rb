efeb156f0669d52009a77a380f55cad1b6e591ce
Michael Waldo <mbwcogic@yahoo.com>
2019-10-18 21:29:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,7 @@ katz_deil = [] # Write your code here.
 def line(katz_deli)
 if []
 puts "The line is currently empty."
-elsif 
-
  else
-   puts (other_deli)
+   puts "The line is currently"(other_deli)
 end
 end

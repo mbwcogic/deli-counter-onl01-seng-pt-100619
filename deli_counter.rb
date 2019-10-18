ee865b4794e750ce69fa48231e45a40a9163c026
Michael Waldo <mbwcogic@yahoo.com>
2019-10-18 22:08:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
 if []
 puts "The line is currently empty."
  else
-   other_deli.each ||
-   puts "The line is currently"= (other_deli)
+   other_deli.each |name|
+   puts "The line is currently" #{name}
 end
 end

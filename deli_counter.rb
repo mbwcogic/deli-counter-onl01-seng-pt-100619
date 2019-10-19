a48d22d1de7c7c4f7aabbd21ffb733be43e18a82
Michael Waldo <mbwcogic@yahoo.com>
2019-10-19 01:57:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,8 @@ puts string
 end
 
 def take_a_number(array, name)
-  array << "name"
-  puts "Welcome, #{"name"}. You are number #{array.length} in line."
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
  
 
